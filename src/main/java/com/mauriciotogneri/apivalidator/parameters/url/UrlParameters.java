8f0c7ac3212f3ff362cd1c62fa7a1d7f88ec737a
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-05-29 23:01:27 +0200

    Code cleanup

--- src/main/java/com/mauriciotogneri/apivalidator/parameters/url/UrlParameters.java
+++ src/main/java/com/mauriciotogneri/apivalidator/parameters/url/UrlParameters.java
@@ -1,11 +1,56 @@
 package com.mauriciotogneri.apivalidator.parameters.url;
 
-import com.mauriciotogneri.apivalidator.parameters.base.ConcatenatedParameters;
+import java.lang.reflect.Field;
 
-public class UrlParameters extends ConcatenatedParameters
+public class UrlParameters
 {
+    private final Object object;
+
     public UrlParameters(Object object)
     {
-        super(object, true);
+        this.object = object;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        Field[] fields = object.getClass().getDeclaredFields();
+
+        for (Field field : fields)
+        {
+            try
+            {
+                Object value = field.get(object);
+
+                if (value != null)
+                {
+                    addValue(field.getName(), value.toString(), builder);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private void addValue(String key, String value, StringBuilder builder)
+    {
+        if (builder.length() == 0)
+        {
+            builder.append("?");
+        }
+        else
+        {
+            builder.append("&");
+        }
+
+        builder.append(key);
+        builder.append("=");
+        builder.append(value);
     }
 }
